e674d2281ac405e12904939c13f438bb82b8117c
roto5296 <hikun5296@gmail.com>
2020-12-16 00:19:43 +0900

    use argparse fix new line

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ import difflib
 from moneyforward import MoneyForward
 from spreadsheet import SpreadSheet
 import threading
+import argparse
+import datetime
 
 def run(year, month, dict, obj):
     dict[str(year) + '/' + str(month)] = obj.get(year, month)
+
+dt_now_jst = datetime.datetime.now(
+    datetime.timezone(datetime.timedelta(hours=9)))
+parser = argparse.ArgumentParser()
+parser.add_argument('--update', action='store_true')
+parser.add_argument('--year', type=int, default=dt_now_jst.year)
+parser.add_argument('--month', type=int, default=dt_now_jst.month)
+parser.add_argument('--period', type=int, default=6)
+args = parser.parse_args()
+period = min(args.period, 12)
+ym_list = [(
+    args.year if args.month - i > 0 else args.year - 1,
+    args.month - i if args.month - i > 0 else args.month + 12 - i)
+    for i in range(period)]
 
 mf = MoneyForward(os.environ['MONEYFORWARD_KEYFILE'])
 if (not(mf.login())):
@@ -13,12 +29,14 @@ if (not(mf.login())):
     sys.exit()
 if len(sys.argv) == 2 and sys.argv[1] == "--update":
     mf.update()
-ss = SpreadSheet(os.environ['SPREADSHEET_KEYFILE'], os.environ['SPREADSHEET_ID'])
-year = 2020
+ss = SpreadSheet(os.environ['SPREADSHEET_KEYFILE'],
+    os.environ['SPREADSHEET_ID'])
 mfdata_dict = {}
 ssdata_dict = {}
-ts1 = [threading.Thread(target=run, args=(year, month, mfdata_dict, mf)) for month in range(7, 13)]
-ts2 = [threading.Thread(target=run, args=(year, month, ssdata_dict, ss)) for month in range(7, 13)]
+ts1 = [threading.Thread(target=run, args=(year, month, mfdata_dict, mf))
+    for (year, month) in ym_list]
+ts2 = [threading.Thread(target=run, args=(year, month, ssdata_dict, ss))
+    for (year, month) in ym_list]
 for t in ts1:
     t.start()
 for t in ts2:
@@ -27,7 +45,7 @@ for t in ts1:
     t.join()
 for t in ts2:
     t.join()
-for month in range(7,13):
+for (year, month) in ym_list:
     print(str(year) + '/' + str(month))
     mfdata = mfdata_dict[str(year) + '/' + str(month)]
     sdata = ssdata_dict[str(year) + '/' + str(month)]
@@ -40,7 +58,8 @@ for month in range(7,13):
         fname = 'diff' + str(month) + '.html'
         d = difflib.HtmlDiff()
         with open(fname, mode='w') as f:
-            f.write(d.make_file([', '.join(map(str, i)) for i in mfdata], [', '.join(map(str, i)) for i in sdata]))
+            f.write(d.make_file([', '.join(map(str, i)) for i in mfdata],
+                [', '.join(map(str, i)) for i in sdata]))
         ss.update(year, month, mfdata)
 del mf
 del ss
