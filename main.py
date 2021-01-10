b3446c29e35d4152d3468c15cfbff9857ff4fc2c
roto5296 <hikun5296@gmail.com>
2021-01-10 21:23:44 +0900

    use mfscraping package and change SpreadSheet

--- main.py
+++ main.py
@@ -1,86 +1,93 @@
-import sys
-import os
-import difflib
-from moneyforward import MoneyForwardSelenium, MoneyForwardRequests
-from spreadsheet import SpreadSheet
-import threading
 import argparse
 import datetime
+import difflib
+import json
+import os
+import sys
+import threading
+
+from mfscraping import MFScraper
+
+from spreadsheet import SpreadSheet
 
 
-def run(year, month, dict, obj, is_selenium=False, lock=None):
-    if is_selenium:
-        lock.acquire()
-    dict[str(year) + '/' + str(month)] = obj.select(year, month)
-    if is_selenium:
-        lock.release()
+def run(year, month, dict, obj):
+    dict[str(year) + "/" + str(month)] = obj.get(year, month)
 
 
 def run2(year, month, t1, t2, mf_dict, ss_dict, ss, lock):
     t1.join()
     t2.join()
     lock.acquire()
-    print(str(year) + '/' + str(month))
-    mfdata = mf_dict[str(year) + '/' + str(month)]
-    sdata = ss_dict[str(year) + '/' + str(month)]
+    print(str(year) + "/" + str(month))
+    mfdata = mf_dict[str(year) + "/" + str(month)]
+    sdata = ss_dict[str(year) + "/" + str(month)]
     if sdata == mfdata:
-        print('SAME')
+        print("SAME")
     elif len(mfdata) == 0:
-        print('MoneyForward No Data')
+        print("MoneyForward No Data")
     else:
-        print('There is diff\nUpdate sheet')
-        fname = 'diff' + str(month) + '.html'
+        print("There is diff\nUpdate sheet")
+        fname = "diff" + str(month) + ".html"
         d = difflib.HtmlDiff()
-        with open(fname, mode='w') as f:
-            f.write(d.make_file(
-                [', '.join(map(str, i)) for i in mfdata],
-                [', '.join(map(str, i)) for i in sdata]
-            ))
+        with open(fname, mode="w") as f:
+            f.write(
+                d.make_file(
+                    [", ".join(map(str, i)) for i in SpreadSheet.dict2ssformat(mfdata)],
+                    [", ".join(map(str, i)) for i in SpreadSheet.dict2ssformat(sdata)],
+                )
+            )
         ss.merge(year, month, mfdata)
     lock.release()
 
 
-dt_now_jst = datetime.datetime.now(
-    datetime.timezone(datetime.timedelta(hours=9)))
+SpreadSheet.dict2ssformat
+
+dt_now_jst = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
 parser = argparse.ArgumentParser()
-parser.add_argument('--update', action='store_true')
-parser.add_argument('--year', type=int, default=dt_now_jst.year)
-parser.add_argument('--month', type=int, default=dt_now_jst.month)
-parser.add_argument('--period', type=int, default=6)
-parser.add_argument('--selenium', action='store_true')
+parser.add_argument("--update", action="store_true")
+parser.add_argument("--year", type=int, default=dt_now_jst.year)
+parser.add_argument("--month", type=int, default=dt_now_jst.month)
+parser.add_argument("--period", type=int, default=6)
 args = parser.parse_args()
 period = min(args.period, 12)
-ym_list = [(
-    args.year if args.month - i > 0 else args.year - 1,
-    args.month - i if args.month - i > 0 else args.month + 12 - i
-) for i in range(period)]
+ym_list = [
+    (
+        args.year if args.month - i > 0 else args.year - 1,
+        args.month - i if args.month - i > 0 else args.month + 12 - i,
+    )
+    for i in range(period)
+]
 
-if args.selenium:
-    mf = MoneyForwardSelenium(os.environ['MONEYFORWARD_KEYFILE'])
+
+mf = MFScraper(**json.loads(os.environ["MONEYFORWARD_KEYFILE"]))
+print("login...")
+if mf.login():
+    print("LOGIN success")
 else:
-    mf = MoneyForwardRequests(os.environ['MONEYFORWARD_KEYFILE'])
-if (not(mf.login())):
+    print("LOGIN fail")
     del mf
     sys.exit()
 if args.update:
-    mf.fetch()
-ss = SpreadSheet(
-    os.environ['SPREADSHEET_KEYFILE'], os.environ['SPREADSHEET_ID']
-)
+    print("update...")
+    if mf.fetch():
+        print("UPDATE success")
+    else:
+        print("UPDATE fail or timeout")
+ss = SpreadSheet(os.environ["SPREADSHEET_KEYFILE"], os.environ["SPREADSHEET_ID"])
 mfdata_dict = {}
 ssdata_dict = {}
-lock1 = threading.Lock()
-lock2 = threading.Lock()
-ts1 = [threading.Thread(target=run, args=(year, month, ssdata_dict, ss))
-       for (year, month) in ym_list]
-ts2 = [threading.Thread(
-    target=run,
-    args=(year, month, mfdata_dict, mf, args.selenium, lock1)
-) for (year, month) in ym_list]
-ts3 = [threading.Thread(
-    target=run2,
-    args=(year, month, t1, t2, mfdata_dict, ssdata_dict, ss, lock2)
-) for (year, month), t1, t2 in zip(ym_list, ts1, ts2)]
+lock = threading.Lock()
+ts1 = [
+    threading.Thread(target=run, args=(year, month, ssdata_dict, ss)) for (year, month) in ym_list
+]
+ts2 = [
+    threading.Thread(target=run, args=(year, month, mfdata_dict, mf)) for (year, month) in ym_list
+]
+ts3 = [
+    threading.Thread(target=run2, args=(year, month, t1, t2, mfdata_dict, ssdata_dict, ss, lock))
+    for (year, month), t1, t2 in zip(ym_list, ts1, ts2)
+]
 for t1, t2, t3 in zip(ts1, ts2, ts3):
     t1.start()
     t2.start()
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,33 +1,82 @@
-import gspread
+import datetime
 import json
+
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
 class SpreadSheet:
     def __init__(self, jsontext, sheet_id):
-        scope = [
-            'https://spreadsheets.google.com/feeds',
-            'https://www.googleapis.com/auth/drive'
-        ]
-        credentials = ServiceAccountCredentials.from_json_keyfile_dict(
-            json.loads(jsontext), scope
-        )
+        scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(jsontext), scope)
         gc = gspread.authorize(credentials)
         self._wb = gc.open_by_key(sheet_id)
 
-    def select(self, year, month):
+    @staticmethod
+    def dict2ssformat(data):
+        convert_data = []
+        for d in data:
+            amount = abs(d["amount"])
+            is_transfer = d["account_from"] and d["account_to"]
+            convert_data.append(
+                [
+                    d["transaction_id"],
+                    d["date"].strftime("%Y-%m-%d"),
+                    d["content"],
+                    amount if d["account_to"] else -amount,
+                    d["account_from"] + "," + d["account_to"]
+                    if is_transfer
+                    else d["account_from"] or d["account_to"],
+                    "振替" if is_transfer else d["lcategory"],
+                    d["mcategory"],
+                    d["memo"],
+                ]
+            )
+        return convert_data
+
+    @staticmethod
+    def ssformat2dict(data):
+        convert_data = []
+        for d in data:
+            amount = int(d[3])
+            accounts = d[4].split(",")
+            is_transfer = d[5] == "振替"
+            convert_data.append(
+                {
+                    "transaction_id": int(d[0]),
+                    "date": datetime.date(*map(int, d[1].split("-"))),
+                    "content": d[2],
+                    "amount": abs(amount),
+                    "account_from": accounts[0]
+                    if is_transfer
+                    else accounts[0]
+                    if amount <= 0
+                    else None,
+                    "account_to": accounts[1]
+                    if is_transfer
+                    else accounts[0]
+                    if amount > 0
+                    else None,
+                    "lcategory": "" if is_transfer else d[5],
+                    "mcategory": d[6],
+                    "memo": d[7],
+                }
+            )
+        return convert_data
+
+    def get(self, year, month):
         sname = str(year - 2000).zfill(2) + str(month).zfill(2)
         try:
             ws = self._wb.worksheet(sname)
         except gspread.exceptions.WorksheetNotFound:
             return []
-        ret = ws.get_all_values()[2:]
-        for tmp in ret:
-            tmp[0] = int(tmp[0])  # transaction id
-            tmp[3] = int(tmp[3])  # price
+        data = ws.get_all_values()[2:]
+        ret = self.ssformat2dict(data)
+        ret = sorted(ret, key=lambda x: (x["date"], x["transaction_id"]), reverse=True)
         return ret
 
     def merge(self, year, month, data, max_num=200):
+        convert_data = self.dict2ssformat(data)
         month_b = 12 if month == 1 else month - 1
         year_b = year - 1 if month == 1 else year
         wss = self._wb.worksheets()
@@ -38,10 +87,8 @@ class SpreadSheet:
         except ValueError:
             snameb = str(year_b - 2000).zfill(2) + str(month_b).zfill(2)
             wsb_index = sheet_names.index(snameb)
-            wst_index = sheet_names.index('template')
+            wst_index = sheet_names.index("template")
             ws = self._wb.duplicate_sheet(
-                wss[wst_index].id,
-                insert_sheet_index=wsb_index,
-                new_sheet_name=sname
+                wss[wst_index].id, insert_sheet_index=wsb_index, new_sheet_name=sname
             )
-        ws.update('A3', data + [[''] * 8] * (max_num - len(data)))
+        ws.update("A3", convert_data + [[""] * 8] * (max_num - len(data)))
