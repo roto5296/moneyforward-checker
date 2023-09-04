f2a6821b8d49eabd6aea62ccf567698206c204cd
roto5296 <hikun5296@gmail.com>
2023-09-04 11:44:49 +0900

    add withdrawal functions

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,10 +1,11 @@
+import asyncio
 import datetime
 import json
 
 import gspread
 import gspread_asyncio
 from google.oauth2.service_account import Credentials
-from mfscraping_asyncio import Account2str, MFTransaction, is_Account, str2Account
+from mfscraping_asyncio import Account, Account2str, MFTransaction, is_Account, str2Account
 
 
 class SpreadSheet:
@@ -15,6 +16,7 @@ class SpreadSheet:
         )
         self._agcm = gspread_asyncio.AsyncioGspreadClientManager(lambda: self._scoped)
         self._sheet_id = sheet_id
+        self._withdrawal = None
 
     async def login(self) -> None:
         agc = await self._agcm.authorize()
@@ -77,6 +79,72 @@ class SpreadSheet:
         ret = self.ssformat2mf(data[2:])
         ret = sorted(ret, reverse=True)
         return ret
+
+    async def get_withdrawal(
+        self,
+    ) -> tuple[
+        dict[tuple[int, int, Account], dict[str, int | datetime.date]], list[str], list[Account]
+    ]:
+        async def inner_get_withdrawal(
+            self: SpreadSheet,
+        ) -> tuple[
+            dict[tuple[int, int, Account], dict[str, int | datetime.date]],
+            list[str],
+            list[Account],
+        ]:
+            try:
+                ws = await self._wb.worksheet("引き落とし")
+            except gspread.exceptions.WorksheetNotFound:
+                return ({}, [], [])
+            data = await ws.get_all_values()
+            ret = {}
+            col = [str2Account(data[0][i]) for i in range(1, len(data[0]), 2)]
+            row = []
+            for d in data[1:]:
+                date_str = d[0]
+                row.append(date_str)
+                tmp = date_str.split("/")
+                year = int(tmp[0])
+                month = int(tmp[1])
+                for i in range(len(col)):
+                    if d[i * 2 + 1] != "":
+                        date_str = d[i * 2 + 2]
+                        tmp = date_str.split("/")
+                        date = datetime.date(int(tmp[0]), int(tmp[1]), int(tmp[2]))
+                        ret.update(
+                            {
+                                (year, month, col[i]): {
+                                    "amount": int(d[i * 2 + 1]),
+                                    "date": date,
+                                }
+                            }
+                        )
+            return (ret, row, col)
+
+        if not self._withdrawal:
+            self._withdrawal = asyncio.create_task(inner_get_withdrawal(self))
+        return await self._withdrawal
+
+    async def update_withdrawal(self, data: dict[Account, dict[str, int | datetime.date]]) -> None:
+        try:
+            ws = await self._wb.worksheet("引き落とし")
+        except gspread.exceptions.WorksheetNotFound:
+            return
+        _, row, col = await self.get_withdrawal()
+        tasks = []
+        for key, value in data.items():
+            if key in col and isinstance(value["date"], datetime.date):
+                i = col.index(key)
+                j = row.index(value["date"].strftime("%Y/%m"))
+                tasks.append(
+                    asyncio.create_task(ws.update_cell(j + 2, i * 2 + 2, value["amount"]))
+                )
+                tasks.append(
+                    asyncio.create_task(
+                        ws.update_cell(j + 2, i * 2 + 3, value["date"].strftime("%Y/%m/%d"))
+                    )
+                )
+        await asyncio.gather(*tasks)
 
     async def merge(
         self, year: int, month: int, data: list[MFTransaction], max_num: int = 200
