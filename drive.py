07f19826c8aec4af9ede6f117819573c1394948b
roto5296 <hikun5296@gmail.com>
2024-08-23 00:19:44 +0900

    use OAuth2.0

--- drive.py
+++ drive.py
@@ -1,16 +1,12 @@
 import io
-import json
 
-from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
 
 
 class Drive:
-    def __init__(self, jsontext):
-        creds = Credentials.from_service_account_info(json.loads(jsontext))
-        scoped = creds.with_scopes(["https://www.googleapis.com/auth/drive"])
-        self._service = build("drive", "v3", credentials=scoped)
+    def __init__(self, cred):
+        self._service = build("drive", "v3", credentials=cred)
 
     def get(self, file_id):
         request = self._service.files().get_media(fileId=file_id)
